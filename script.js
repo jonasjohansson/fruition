f98aee1e624ed5c4e1e94487986c13bff0a06d66
Jonas Johansson <jns.johansson@gmail.com>
2021-08-09 21:22:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-window.onload = function () {
-	// let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
-	// document.body.classList.add('dark');
-	// __console.environment.ThemeStore.setState({ mode: 'dark' });
-	onDark();
-};
+// let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
+// document.body.classList.add('dark');
+// __console.environment.ThemeStore.setState({ mode: 'dark' });
 console.log('Loaded from www.jonasjohansson.se');
