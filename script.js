081a0919c09bc78d7fdee75042acf727416aee31
Jonas Johansson <jns.johansson@gmail.com>
2021-08-09 21:18:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 window.onload = function () {
+	let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
 	document.body.classList.add('dark');
+	__console.environment.ThemeStore.setState({ mode: 'dark' });
 };
+console.log('Loaded from www.jonasjohansson.se');
