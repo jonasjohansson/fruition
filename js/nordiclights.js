db2b4c6ff8074f9a915938f36a5fbd64960a0230
Jonas Johansson <jns.johansson@gmail.com>
2022-01-05 23:55:47 +0100

    upd

--- js/nordiclights.js
+++ js/nordiclights.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 	// let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
-	// document.body.classList.add('dark');
-	// __console.environment.ThemeStore.setState({ mode: 'dark' });
+	document.body.classList.add('dark')
+	__console.environment.ThemeStore.setState({ mode: 'dark' })
 })
 console.log('Loaded from www.jonasjohansson.se')
