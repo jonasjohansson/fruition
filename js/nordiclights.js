88471c1d7b6373e1b781e9e3e6a71622e5cb8062
Jonas Johansson <jns.johansson@gmail.com>
2022-01-06 00:17:06 +0100

    Update nordiclights.js

--- js/nordiclights.js
+++ js/nordiclights.js
@@ -2,5 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
 	// let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
 	document.body.classList.add('dark')
 	__console.environment.ThemeStore.setState({ mode: 'dark' })
+	for (let elem of document.querySelectorAll('div')) {
+		if (elem.getAttribute('style')) {
+			if (elem.getAttribute('style').indexOf('border-top:') != -1) {
+				elem.removeProperty('border-top')
+			} else {
+			}
+		}
+	}
 })
 console.log('Loaded from www.jonasjohansson.se')
