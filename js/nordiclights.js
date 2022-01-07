206924ca547be169a524c8523ad9f884b82dc948
Jonas Johansson <jns.johansson@gmail.com>
2022-01-07 09:16:09 +0100

    Update nordiclights.js

--- js/nordiclights.js
+++ js/nordiclights.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	// let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
 	// document.body.classList.add('dark')
 	// __console.environment.ThemeStore.setState({ mode: 'dark' })
+	document.title = 'Nordic Lights'
 	for (let elem of document.querySelectorAll('div')) {
 		if (elem.getAttribute('style')) {
 			if (elem.getAttribute('style').indexOf('border-top:') != -1) {
