86bf7c2e192fa2838d28f84f4ef651ece656287b
Jonas Johansson <jns.johansson@gmail.com>
2022-01-11 22:14:45 +0100

    Update nordiclights.js

--- js/nordiclights.js
+++ js/nordiclights.js
@@ -9,21 +9,21 @@ console.log('Loaded from www.jonasjohansson.se')
 function update() {
 	setTimeout(function () {
 		document.title = 'Nordic Lights'
-		for (let elem of document.querySelectorAll('div')) {
-			if (elem.getAttribute('style')) {
-				if (elem.getAttribute('style').indexOf('border-top:') != -1) {
-					elem.removeProperty('border-top')
-				} else {
-				}
-			}
-		}
-		document.querySelectorAll('video').forEach((video) => {
-			video.setAttribute('playsinline', '')
-			video.setAttribute('muted', true)
-			video.setAttribute('loop', true)
-			video.setAttribute('controls', false)
-			video.play()
-		})
+		// for (let elem of document.querySelectorAll('div')) {
+		// 	if (elem.getAttribute('style')) {
+		// 		if (elem.getAttribute('style').indexOf('border-top:') != -1) {
+		// 			elem.removeProperty('border-top')
+		// 		} else {
+		// 		}
+		// 	}
+		// }
+		// document.querySelectorAll('video').forEach((video) => {
+		// 	video.setAttribute('playsinline', '')
+		// 	video.setAttribute('muted', true)
+		// 	video.setAttribute('loop', true)
+		// 	video.setAttribute('controls', false)
+		// 	video.play()
+		// })
 		document.querySelector("link[rel='shortcut icon']").href = 'https://jonasjohansson.github.io/chorus/favicon.ico'
 		document.querySelector("link[rel='apple-touch-icon']").href = 'https://jonasjohansson.github.io/chorus/favicon.png'
 	}, 1000)
