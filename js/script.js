b77bebc14385b9a0cf80b43e4fdb8c67648fafec
Jonas Johansson <jns.johansson@gmail.com>
2022-01-15 00:59:56 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,5 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	// let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
 	// document.body.classList.add('dark');
 	// __console.environment.ThemeStore.setState({ mode: 'dark' });
-});
-console.log('Loaded from www.jonasjohansson.se');
+	document.classList.remove('loading')
+})
+console.log('Loaded from www.jonasjohansson.se')
+
+document.classList.add('loading')
